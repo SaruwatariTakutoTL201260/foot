38bcb650f7662c0965dc4fbdb38b8651880018dd
saruwataritakuto <t.saruwatari@shift7.jp>
2023-05-04 22:24:35 +0900

    試合日程

---
+++ src/Constant/OpenApiConstant.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Constant;
+
+/**
+ * OpenApiコンスタント
+ * 
+ * shellにてapi.footballを呼び出す際の手数定義
+ * 
+ * @package App/Constant
+ */
+class OpenApiConstant
+{
+    /**
+     * ApiKey
+     * 
+     * @var string
+     */
+    public const OPEN_API_KEY = '357f22fd54c4a1d6f560130e341b1db3';
+}
--- src/Shell/HelloShell.php
+++ src/Shell/HelloShell.php
@@ -3,11 +3,23 @@ declare(strict_types=1);
 namespace App\Shell;
 
 use Cake\Console\Shell;
+use Cake\Http\Client;
+use App\Constant\OpenApiConstant;
 
 class HelloShell extends Shell
 {
     public function main()
     {
-        $this->out('Hello world.');
+        $http = new Client();
+
+        // APIsportsのステータスエンドポイントにGETリクエストを送信
+        $response = $http->get('https://v3.football.api-sports.io/status', [], [
+            'headers' => [
+                'x-apisports-key' => OpenApiConstant::OPEN_API_KEY, // APIsportsの認証キーをヘッダーに追加
+            ]
+        ]);
+
+        // レスポンスボディを出力
+        echo $response->getBody();
     }
 }
